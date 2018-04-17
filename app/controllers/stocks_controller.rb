cef18e4f132337333c5db83bbb5b5819be21d879
Ken Corley <kwc@Kens-MacBook-Pro.local>
2018-04-17 14:27:05 -0600

    Added error handling and exception catching for look up feature

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_stock_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_stock_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = 'Could not locate that ticker symbol. Please try again.'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = 'Please enter a ticker symbol to look up.'
+      redirect_to my_portfolio_path
+    end
   end
 
 end
